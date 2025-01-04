1bf9c11e7405b467f197dd31f31db3db508e1c7e
felipeversiane <felipeversiane09@gmail.com>
2025-01-04 17:19:18 -0300

    fix: recommit

--- app/utils/index.tsx
+++ app/utils/index.tsx
@@ -2,5 +2,3 @@ import continueWithSocialAuth from './continue-with-social-auth';
 
 export const continueWithGoogle = () =>
 	continueWithSocialAuth('google-oauth2', 'google');
-export const continueWithFacebook = () =>
-	continueWithSocialAuth('facebook', 'facebook');
--- src/account/admin.py
+++ src/account/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import *
 
-# admin.site.register(YourModel)
+admin.site.register(UserAccount)
 
 
